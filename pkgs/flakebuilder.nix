b2da3252a6e860c1e2737c3b969dc82748af55be
Derrik Diener <soltros@proton.me>
2026-09-10 02:33:29 -0400

    Update Flakebuilder package pin

--- pkgs/flakebuilder.nix
+++ pkgs/flakebuilder.nix
@@ -10,8 +10,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "soltros";
     repo = "Flakebuilder";
-    rev = "72d54d9";
-    hash = "sha256-xSfYpHjywOruNYa6f61pQQoeLe6BkJiywQhm3SSJE6E=";
+    rev = "f9cc87e";
+    hash = "sha256-ZzsdyL4wc51KO1AqZtQcdE3ABPJkkxa56t9IgBJjcT4=";
   };
 
   vendorHash = "sha256-uwBJAqN4sIepiiJf9lCDumLqfKJEowQO2tOiSWD3Fig=";
