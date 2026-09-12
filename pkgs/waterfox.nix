8f1395ab90e69f757c2fb9f85d0f9fa0dcbc4b52
Derrik Diener <soltros@proton.me>
2026-09-12 11:42:21 -0400

    Update Waterfox to 6.7.3

--- pkgs/waterfox.nix
+++ pkgs/waterfox.nix
@@ -18,16 +18,16 @@
 }:
 
 let
-  version = "6.7.1.1";
+  version = "6.7.3";
 
   sources = {
     x86_64-linux = {
       url = "https://cdn.waterfox.com/waterfox/releases/${version}/Linux_x86_64/waterfox-${version}.tar.bz2";
-      hash = "sha256-eJKCbcHfTU3LG/9bUm0guzH0LpDc2dSuoohOWVlBwd4=";
+      hash = "sha256-2Pd9NDlHZDWLSsNlRzOJtGuArvWRVr+eD3K59hys0i8=";
     };
     aarch64-linux = {
       url = "https://cdn.waterfox.com/waterfox/releases/${version}/Linux_aarch64/waterfox-${version}.tar.bz2";
-      hash = "sha256-q3/hTI45Faeg3ueM5z0IoowtB8Lnyu4cdP8fCgbTnio=";
+      hash = "sha256-mhW/4daAG3C3Jpmde3E/ZiqqROtJmhGQG3UoPTCqmJU=";
     };
   };
 
